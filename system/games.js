59ed7552be45ab8009d3de27a43f92315c30c0ac
cxon <145627982+lolrender@users.noreply.github.com>
2026-05-30 11:09:07 -0400

    Update games.js

--- system/games.js
+++ system/games.js
@@ -1,17 +1,14 @@
 const games = [
-
-{
-  id: "motox3m-winter",
-  title: "MotoX3M Winter",
-  tag: "MotoX3M Winter",
-  image: "games/motox3m-winter/splash.jpeg"
-},
-
-{
-  id: "motox3m-spooky",
-  title: "MotoX3M Spooky",
-  tag: "MotoX3M Spooky",
-  image: "games/motox3m-spooky/splash.jpeg"
-}
-
+  {
+    id: "motox3m-winter",
+    title: "Moto X3M Winter",
+    tag: "Racing",
+    image: "games/motox3m-winter/splash.jpeg"
+  },
+  {
+    id: "motox3m-spooky",
+    title: "Moto X3M Spooky",
+    tag: "Racing",
+    image: "games/motox3m-spooky/splash.jpeg"
+  }
 ];
